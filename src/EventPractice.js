9792901053ebfc5b07dc505929e38f4776204999
qorlgns1 <rlgns0610@gmail.com>
2021-08-22 23:45:56 +0900

    class 형태의 이벤트 핸들링

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
+    username: '',
     message: '',
   };
 
   handleChange = (e) => {
-    this.setState({ message: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: '' });
+    alert(this.state.username + ': ' + this.state.message);
+    this.setState({
+      username: '',
+      message: '',
+    });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleClick();
+    }
   };
 
   render() {
@@ -20,10 +32,19 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요."
           value={this.state.value}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
