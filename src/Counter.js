ad7e16d7947580dfbb866025c30c09f9a4c3e2f7
qorlgns1 <rlgns0610@gmail.com>
2021-08-22 22:49:40 +0900

    함수형 컴포넌트에서 useState 사용하기

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
     const { number, fixedNumber } = this.state;
     return (
@@ -17,8 +13,15 @@ class Counter extends Component {
         <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            //this.setStatefmf tkdydgkdu state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('방금 setState가 호출되었습니다.');
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
